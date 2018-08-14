248aa962b5768963327667c089fe4a383523ed7e
Andrew M. Zhang <andrewmzhang@berkeley.edu>
2018-08-14 16:28:44 -0700

    Added test.py

--- test.py
+++ test.py
@@ -1,13 +1,33 @@
-import pywren
-import time
+import numpy as np
+from sklearn import preprocessing
+
+import math
+import boto3
+import pickle
+
+HASH = 1000000
+
+converter = {}
+for i in range(14):
+    converter[i] =  lambda s: float(s.strip() or 0)
+for i in range(14, 40):
+    converter[i] = lambda s: hash(s) % HASH
+
+data = np.loadtxt('testset.txt', converters=converter, delimiter="\t")
+
+ys = data[:, 0]
+xs_dense = data[:, 1:14]
+xs_sparse = data[:, 14:]
+
+min_max_scaler = preprocessing.MinMaxScaler()
+xs_dense = min_max_scaler.fit_transform(xs_dense)
+xs_dense = np.column_stack([np.ones((xs_dense.shape[0])), xs_dense]) # N by (D+1)
+
+batch = (xs_dense, xs_sparse, ys)
 
 
-def my_function(x):
-    return time.time()
+out = open("testset.data", "wb")
 
+pickle.dump(batch, out)
+out.close()
 
-a = time.time()
-wrenexec = pywren.default_executor()
-futures = wrenexec.map(my_function, range(10))
-
-[print(t - a) for t in pywren.get_all_results(futures)]
