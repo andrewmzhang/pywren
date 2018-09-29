3027d82f71163e6649e069ead0cd6ed3394531db
Ubuntu <ubuntu@ip-172-31-7-24.us-west-2.compute.internal>
2018-09-29 08:32:28 +0000

    Finished 5 iterations

--- five.py
+++ five.py
@@ -1,4 +1,6 @@
-import queue
+
+# Iteration 4
+import redis
 import sys
 import pywren
 import numpy as np
@@ -7,22 +9,22 @@ import boto3
 import pickle
 import time
 import random
-
 import json
-
 from threading import Event
 from threading import Thread
 from sklearn import preprocessing
 from scipy import sparse
 from functools import reduce
+from multiprocessing.pool import ThreadPool
+from sklearn.preprocessing import OneHotEncoder
 
 HASH = 524288
 
 lr = 0.0001            # Learning rate
 minibatch_size = 20    # Size of minibatch
-batch_size = 20      # Size of whole batch
+batch_size = 1000      # Size of whole batch
 total_batches = 1000   # Entire number of batches in dataset
-batch_file_size = 10    # Number of lambda
+batch_file_size = 10    # Number of minibatches per lambda
 num_lambdas = 10
 total_time = 60
 log = False
@@ -30,8 +32,9 @@ fname = "def.txt"
 outf = None
 
 
-from multiprocessing.pool import ThreadPool
-from sklearn.preprocessing import OneHotEncoder
+redis_host = "andyredis.snwohz.ng.0001.usw2.cache.amazonaws.com"
+redis_port = 6379
+
 
 fin = []
 
@@ -44,24 +47,42 @@ def prediction(param_dense, param_sparse, x_dense, x_sparse):
     return out
 
 def store_update(update):
+    redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
     t0 = time.time()
-    s3 = boto3.resource('s3')
-    key = 'gradient_%d/g_%d' % (np.random.randint(1, 9), random.randint(1, 10))
+    key = 'gradient_%d' % np.random.randint(1, 9)
     datastr = pickle.dumps(update)
     t1 = time.time()
-    def up_func(s3, key, datastr):
-        s3.Bucket('camus-pywren-489').put_object(Key=key, Body=datastr)
-    thread = Thread(target = up_func, args = (s3, key, datastr ))
+    def up_func(key, datastr):
+        redis_client.lpush(key, datastr)
+    thread = Thread(target = up_func, args = (key, datastr ))
     thread.start()
     # Return reserialzie, upload
     return t1 - t0, time.time() - t1
+
+def sparse_ind(xpy):
+    (x_dense, x_idx, y) = xpy
+    lst = []
+    for idx in x_idx:
+        lst.append(idx)
+    return lst
+
+# This is only for mget with redis
+def get_model(prefix, lst):
+    keys = [prefix + ":" + str(x) for x in lst]
+    redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
+    sparse_values = redis_client.mget(keys)
+    param_sparse = sparse.lil_matrix((HASH, 1))
+    i = 0
+    for x in lst:
+        param_sparse[x][0] = sparse_values[i]
+        i += 1
+    param_dense = get_data('model-dense')
+    return (param_dense, param_sparse)
 
 
 # Map function for pywren main
 def gradient_batch(xpys):
     start = time.time()
-    model = get_data('model')
-    fetch_model_time = time.time() - start
     xpys = xpys.split(" ")
     reser, upload, model_fetch, model_deser = 0, 0, 0, 0
 
@@ -69,34 +90,30 @@ def gradient_batch(xpys):
     for xpy in xpys:
         det = {}
         for mb in get_minis(xpy, det):
-            if iterno > 0:
-                det['loop_time'] = time.time() - prev_start_time
-            prev_start_time = time.time()
+            sparse_idx = sparse_ind(mb)
+            model = get_model('model-sparse', sparse_idx)
 
             left, right, det = gradient(mb, model, det)
             out = (np.sum(left, axis=0), np.sum(right, axis=0))
 
             if iterno == 0:
-                det['init_model_fetch'] = fetch_model_time
+                det['init_model_fetch'] = 0
 
             if iterno > 0:
-
                 det['reserialize_time'] = reser
                 det['upload_time'] = upload
                 det['model_deser_time'] = model_deser
                 det['model_fetch_time'] = model_fetch
             det['subtime'] = time.time()
-            to_store = [det, out[0], out[1]]
+            to_store = [det, out[0], out[1], sparse_idx]
             reser, upload = store_update(to_store)
-            model, model_deser, model_fetch = get_data('model', True)
-            iterno += 1
-            if True:
+            if time.time() - start > 240:
                 break;
-        if True:
+            iterno += 1
+        if time.time() - start > 240:
             break;
 
     to_store[0]['lambda_time_alive'] = time.time() - start
-    to_store[0]['total_iters'] = iterno
     return to_store
 
 # convert matrices to dense and sparse halves
@@ -153,12 +170,12 @@ def reduce_sum(lst):
     l = [l[0] for l in lst]
     r = [l[1] for l in lst]
     left_grad, right_grad = np.sum(np.vstack(l), axis=0), np.sum(np.vstack(r), axis=0)
-    return left_grad, right_grad
+    return left_grad, right_grad, lst[2]
 
 # Log loglikelihood func
 def loglikelihood(test_data, model):
     xs_dense, xs_sparse, ys = test_data
-    param_dense, param_sparse = model
+    param_dense, param_sparse, sparse_idx = model
     preds = prediction(param_dense, param_sparse, xs_dense, xs_sparse)
     ys_temp = ys.reshape((-1, 1))
     cors = np.around(preds)
@@ -181,24 +198,42 @@ def get_minis(key, det):
 
 # AWS helper function
 def get_data(key, a = False):
-    s3 = boto3.resource('s3')
-    t0 = time.time()
-    obj = s3.Object('camus-pywren-489', key)
-    t1 = time.time()
-    body = obj.get()['Body'].read()
-    data = pickle.loads(body)
-    # Return data, time to deseralize, time to fetch
-    if a:
-        return data, time.time() - t1, t1 - t0
-    return data
+    print("Getting", key)
+
+    if '3k' in key or 'proper' in key:
+        s3 = boto3.resource('s3')
+        t0 = time.time()
+        obj = s3.Object('camus-pywren-489', key)
+        t1 = time.time()
+        body = obj.get()['Body'].read()
+        data = pickle.loads(body)
+        # Return data, time to deseralize, time to fetch
+        if a:
+            return data, time.time() - t1, t1 - t0
+        return data
+    else:
+        cli = redis.StrictRedis(host="andyredis.snwohz.ng.0001.usw2.cache.amazonaws.com", port=6379, db=0)
+        string = cli.get(key)
+        if a:
+            return pickle.loads(string), 0, 0
+        return pickle.loads(string)
 
 def store_model(model):
-    param_dense, param_sparse = model
-    s3 = boto3.resource('s3')
-    key = 'model'
-    model = (param_dense, param_sparse)
+    param_dense, param_sparse, sparse_idx = model
+    redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
+    key = 'model-dense'
+    model = param_dense
     datastr = pickle.dumps(model)
-    s3.Bucket('camus-pywren-489').put_object(Key=key, Body=datastr)
+    redis_client.set(key, datastr)
+    if sparse_idx is None:
+        return
+    prefix = 'model-sparse:'
+    key_values = {}
+    for x in sparse_idx:
+        key = prefix + str(x)
+        key_values[key] = x
+    redis_client.mset(key_values)
+      
 
 index = 1
 def get_minibatches(num, over=2):
@@ -210,7 +245,7 @@ def get_minibatches(num, over=2):
         begin, end = index, index + over
         minis = []
         for b in range(begin, end):
-            key = 'mini20-' + str(b)
+            key = 'proper-' + str(b)
             minis.append(key)
         index = index + over
         group.append(' '.join(mini for mini in minis))
@@ -219,23 +254,35 @@ def get_minibatches(num, over=2):
 
 def update_model(model, gradient):
     global lr
-    left, right = gradient
+    left, right, sparse_idx = gradient
     left = np.reshape(left, (14, 1))
     right = np.reshape(right, (HASH, 1))
     param_dense, param_sparse = model
 
     param_dense = np.add(param_dense, np.multiply(lr, left))
     param_sparse = sparse.lil_matrix(np.add(param_sparse.todense(), np.multiply(lr, right)))
-    return (param_dense, param_sparse)
+    return (param_dense, param_sparse, sparse_idx)
 
 def init_model():
     param_dense = np.zeros((14, 1))
     param_sparse = sparse.lil_matrix((HASH, 1))
-    model = (param_dense, param_sparse)
+    model = (param_dense, param_sparse, None)
+
+    redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
+    redis_client.flushall()
+    kv = {}
+    for i in range(HASH):
+        kv["model-sparse:" + str(i)] = 0.0
+        if len(kv) > 100000:
+            print(i)
+            redis_client.mset(kv)
+            kv = {}
+    redis_client.mset(kv)
+
     return model
 
 def get_test_data():
-    test_key = "mini20-0"
+    test_key = "3k-0"
     x_dense_test, x_idx_test, y_test = get_data(test_key)
     x_sparse_test = sparse.lil_matrix((x_dense_test.shape[0], HASH))
     for i in range(x_dense_test.shape[0]):
@@ -244,7 +291,6 @@ def get_test_data():
 
 def get_local_test_data():
     f = open("testset.data", "rb")
-    f.seek(0)
     x_dense_test, x_idx_test, y_test = pickle.load(f)
     x_sparse_test = sparse.lil_matrix((x_dense_test.shape[0], HASH))
     for i in range(x_dense_test.shape[0]):
@@ -265,23 +311,22 @@ def m(f):
         return [], f
 
 
-grad_q = queue.Queue()
+grad_q = []
 
 def fetch_thread(i):
+    redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
     global outf
     global grad_q
-    s3 = boto3.resource('s3')
-
-    my_bucket = s3.Bucket('camus-pywren-489')
+    global log
 
     num = 0
     start_time = time.time()
     while time.time() - start_time < total_time:
-        lst = my_bucket.objects.filter(Prefix='gradient_%d/' % i).all()
+        lst = redis_client.lrange("gradient_%d" % i, 0, -1)
+        redis_client.ltrim("gradient_%d" % i, 0, -1)
         for object in lst:
             s = time.time()
-            obj = object.get()
-            grad = pickle.loads(obj['Body'].read())
+            grad = pickle.loads(object)
 
             for key, value in grad[0].items():
                 if key == "subtime" and log:
@@ -290,8 +335,8 @@ def fetch_thread(i):
                     if log:
                         outf.write("%s %f\n" % (key, value))
 
-            grad_q.put(grad[-2:])
-            object.delete()
+            grad_q.append(grad[-3:])
+            #object.delete()
             num += 1
             #print("Fetched: %d, took: %f, thread: %d. Sit time: %f" % (num, time.time() - s, i, time.time() - grad[0]['subtime']))
             if time.time() - start_time > total_time:
@@ -302,10 +347,7 @@ def error_thread(model):
     global grad_q
     global log
     global fname
-    global index
 
-    s3 = boto3.resource('s3')
-    my_bucket = s3.Bucket('camus-pywren-489')
     num = 0
     print("Starting error thread")
     start_time = time.time()
@@ -318,30 +360,24 @@ def error_thread(model):
     if True:
         print(fname[:-4] + ".pkl")
         f = open(fname[:-4] + ".pkl", 'wb')
-    time_model_lst = []
     while time.time() - start_time < total_time:
-
-        if not grad_q.empty():
-            sz = grad_q.qsize()
-            grads = []
-            for _ in range(sz):
-                grad = grad_q.get()
-                grads.append(grad)
-                grad_q.task_done()
+        grads = grad_q[:]
+        grad_q = []
+        if len(grads) > 0:
             bg = reduce_sum(grads)
-            model = update_model(model, bg)
+            model = update_model((model[0], model[1] ), bg)
             store_model(model)
             num += len(grads)
-            #error = loglikelihood(test_data, model)
+            error = loglikelihood(test_data, model)
             curr_time = time.time() - start_time
-            print("[ERROR_TASK]", curr_time, 0, "this many grads:", num, "Sec / Grad:", (time.time() - start_time)/ num)
+            print("[ERROR_TASK]", curr_time, error, "this many grads:", num, "Sec / Grad:", (time.time() - start_time)/ num)
+            outf.write("[ERROR_TASK]", curr_time, error, "this many grads:", num, "Sec / Grad:", (time.time() - start_time)/ num)
             if True:
                 print("dumping")
                 pickle.dump((curr_time, model), f)
-                print("dump done")
                 saves += 1
 
-    print("Saves: ", saves, "Index:", index)
+    print("Saves: ", saves)
     if True:
         large_test = get_local_test_data()
         f.close()
@@ -373,7 +409,7 @@ def main(thread, log=False):
 
     # start jobs
     minibatches = get_minibatches(fin)
-    futures = start_batch(minibatches)
+    fs.extend(start_batch(minibatches))
     fin = 0
     iter = 0
 
@@ -386,14 +422,28 @@ def main(thread, log=False):
         res = []
         ded = []
 
-        pywren.get_all_results(futures)
+        #print("Start pool")
+        t = time.time()
+        pool = ThreadPool(num_lambdas)
+        resa = []
+        resa = pool.map(m, fs)
+        #print("End pool: %f" % (time.time() - t))
 
-        fin = len(futures)
+        res = []
+        for a in resa:
+            if a != None:
+                fs.remove(a[1])
+                res.append(a[0])
+                print("FIN", a)
+
+
+        fin = len(res)
         iter += fin
         if fin > 0:
             print("Processed: %d" % fin)
             minibatches = get_minibatches(fin)
-            futures = start_batch(minibatches)
+            # Run Map Reduce with Pywren
+            fs.extend(start_batch(minibatches))
     print("Main thread has stopped")
 
 
@@ -405,6 +455,7 @@ if __name__ == "__main__":
     global log
     global fname
     log = False
+    redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
     if len(sys.argv) >= 2:
         data = json.loads(sys.argv[1])
         total_time = float(data['total_time'])
@@ -422,17 +473,16 @@ if __name__ == "__main__":
 
         print("Logging was requested with output file: %s and rate: %f" % (fname, lr))
 
-    s3 = boto3.resource('s3')
 
-    my_bucket = s3.Bucket('camus-pywren-489')
-
+    print("Starting")
     for i in range(1, 9):
-        string = "gradient_%d/" % i
-        for object in my_bucket.objects.filter(Prefix=string).all():
-            object.delete()
+        string = "gradient_%d" % i
+        redis_client.delete(string)
 
     time.sleep(5)
+    print("Init model")
     model = init_model()
+    print(len(model))
     store_model(model)
 
     thread = Thread(target=error_thread, args=(model, ))
--- loss.py
+++ loss.py
@@ -6,15 +6,17 @@ config = {
     "total_time": 500,
 }
 
-for lr in [ 0.0005 ]:
-    fname = "tmp-%f-loss.txt" % lr
+
+
+programs = ['', 'one.py', 'two.py', 'a.py', 'three.py', 'four.py', 'five.py']
+
+for a in [ (0.0005,1), (0.0005,2), (0.0005,3),(0.0005,4), (0.0005,5)]:
+    lr, it = a
+    fname = "tmp-%f-loss-%d.txt" % (lr, it)
     config['fname'] = fname
     config['lr'] = lr
-#    if lr == 0.0005:
-#        config['total_time'] = 1200
-#    else:
-#        config['total_time'] = 600
-    cmd = "python3 a.py '%s'" % json.dumps(config)
+    config['total_time'] = 1200
+    cmd = "python3 %s '%s' " % (programs[it], json.dumps(config))
     print(cmd)
     os.system(cmd)
     os.system("rm *.pkl")
